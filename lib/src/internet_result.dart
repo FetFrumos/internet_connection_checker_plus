953b42a3bd0652a96c6980ed931a94f93c818c0a
Andrii <ber911@gmail.com>
2024-11-02 15:31:29 +0200

    updated InternetResult

--- lib/src/internet_result.dart
+++ lib/src/internet_result.dart
@@ -1,12 +1,13 @@
 part of '../internet_connection_checker_plus.dart';
 
-
 class InternetResult extends Equatable {
   final List<ConnectivityResult> netResult;
   final InternetStatus status;
 
-  const InternetResult({required this.netResult, required this.status});
+  const InternetResult({required this.status, required this.netResult});
 
   @override
   List<Object?> get props => [status, netResult];
+
+  factory InternetResult.init() => InternetResult(netResult: [], status: InternetStatus.disconnected);
 }
